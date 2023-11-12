79958f0e50859473b17c2217e3ad1da2bea10f3e
Ugnius Silevicius <90143876+stasyskirstukas@users.noreply.github.com>
2023-11-12 15:24:15 +0200

    a

--- Calculator/app/build.gradle.kts
+++ Calculator/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.calculator"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.calculator"
@@ -38,6 +38,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
@@ -59,6 +60,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,25 +1,44 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.annotation.ContentView
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.calculator.ui.theme.CalculatorTheme
+import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-
-
+        binding.txtValue.text = "0"
     }
+
+    fun buttonAction(view: View)
+    {
+        if(view is Button)
+        {
+            if(binding.txtValue.text == "0")
+            {
+                binding.txtValue.text = view.text
+                println("aaaa")
+            }
+            else binding.txtValue.append(view.text)
+        }
+    }
+
+    fun btnAllClear(view: View)
+    {
+        binding.txtValue.text = "0"
+    }
+    fun btnBackspace(view: View)
+    {
+        binding.txtValue.text = binding.txtValue.text.dropLast(1)
+        if(binding.txtValue.text.length == 0) binding.txtValue.text = "0"
+    }
+    fun btnEqual(view: View) {}
 
 }
